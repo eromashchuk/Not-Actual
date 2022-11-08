0cb65313eecfef8d3299e3107ba9d58f3d9c1104
e.romaschuk <shumak@gmail.com>
2022-11-08 18:19:14 +0300

    Add Home Work 13_8_19 for Unit 13

---
+++ Task13_8_19.py
@@ -0,0 +1,21 @@
+bilet_n = int(input("Введите количество билетов: "))
+discount = 0.9
+total_price = 0
+try:
+    for i in range(1, bilet_n + 1):
+        age = int(input(f"Введите возраст для {i} билета: "))
+        if 0 <= age <= 18:
+            price = 0
+        elif 18 < age <= 25:
+            price = 990
+        else:
+            price = 1395
+        total_price += price
+        i = i + 1
+
+finally:
+    if bilet_n > 3:
+        print(f'Общая стоимость: {total_price} руб.')
+        print(f'Стоимость со скидкой 10%: {total_price * 0.9} руб.')
+    else:
+        print(f'Общая стоимость: {total_price} руб.')
