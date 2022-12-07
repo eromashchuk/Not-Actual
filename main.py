97dfbfb0860da0d738986aa124421d0274699c4d
e.romaschuk <shumak@gmail.com>
2022-12-07 23:55:51 +0300

    ALL

--- main.py
+++ main.py
@@ -622,3 +622,32 @@ from numpy._distributor_init import filename
 #
 # # запускаем алгоритм на левой и правой границе
 # print(binary_search(array, element, 0, 99))
+
+from decorators import do_twice, debug
+
+@do_twice
+def test_twice_without_params():
+    print("Этот вызов без параметров")
+
+
+@do_twice
+def test_twice_2_params(str1, str2):
+    print("В этой функции 2 параметра - {0} и {1}".format(str1, str2))
+
+
+@do_twice
+def test_twice(str):
+    print("Этот вызов возвращает строку {0}".format(str))
+    return "Done"
+
+decorated_value = test_twice("single")
+
+@debug
+def age_passed(name, age=None):
+  if age is None:
+    return "Необходимо передать значение age"
+  else:
+    return "Аргументы по умолчанию заданы!"
+
+age_passed("Роман")
+age_passed("Роман", age=21)
